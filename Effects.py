5a7d53a29a4f763f28f9b9ab3f0e0a4af77e7bbb
Jerry Wu <jerrywuzijie@gmail.com>
2021-11-24 16:48:00 -0500

    adding effects.py for all classes corresponding to different effects

---
+++ Effects.py
@@ -0,0 +1,29 @@
+import scipy
+
+
+class Effect:
+    def __init__(self,):
+        pass
+
+    def filter(x):
+        pass
+
+
+class AM(Effect):
+    def __init__(self):
+        pass
+
+
+class Delay(Effect):
+    def __init__(self):
+        pass
+
+
+class LPF(Effect):
+    def __init__(self):
+        pass
+
+
+class HPF(Effect):
+    def __init__(self):
+        pass
