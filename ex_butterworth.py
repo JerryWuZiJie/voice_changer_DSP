94becc76f929bebe06d09060742e7e14d7c0504d
Jerry Wu <jerrywuzijie@gmail.com>
2021-11-25 16:45:47 -0500

    create a template for example, and modified the betterworth filter example

--- ex_butterworth.py
+++ ex_butterworth.py
@@ -1,57 +1,10 @@
 import numpy as np
-import struct
-import pyaudio
 import Effects
-
-
-BLOCKLEN = 1024    # Number of frames per block
-WIDTH = 2          # Bytes per sample
-CHANNELS = 1       # Number of channels
-RATE = 8000        # Sampling rate in Hz (samples/second)
-
-# Parameters
-T = 10       # Total play time (seconds)
-
-NumBlocks = int(T * RATE / BLOCKLEN)
+from ex_template import mic_in_spkr_out
 
 # make 6 order band-pass butterworth filter using scipy.signal
-f1 = 200
-f2 = 1000
-nyq = 0.5 * RATE
-low = f1 / nyq
-high = f2 / nyq
-butter_filter = Effects.ButterWorth(6, [low, high], btype='bandpass')
+lower_frequency = 200
+upper_frequency = 1000
+order = 6
 
-# Open the audio output stream
-p = pyaudio.PyAudio()
-PA_FORMAT = p.get_format_from_width(WIDTH)
-stream = p.open(format=PA_FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                output=True,
-                frames_per_buffer=256)
-
-print('Playing for %f seconds ...' % T)
-
-
-# Loop through blocks
-for i in range(NumBlocks):
-
-    input_bytes = stream.read(BLOCKLEN, exception_on_overflow=False)
-    x = struct.unpack('h'*BLOCKLEN, input_bytes)
-    y = butter_filter.lfilter(x)
-
-    y = np.clip(y.astype(np.int), -32768, 32767)
-
-    # Convert numeric list to binary data
-    output_bytes = struct.pack('h' * BLOCKLEN, *y)
-
-    # Write binary data to audio output stream
-    stream.write(output_bytes, BLOCKLEN)
-
-print('* Finished')
-
-stream.stop_stream()
-stream.close()
-p.terminate()
+mic_in_spkr_out(Effects.ButterWorth, frequency=np.array(lower_frequency, upper_frequency), order=order, btype="bandpass")
---
+++ ex_template.py
@@ -0,0 +1,50 @@
+import numpy as np
+import struct
+import pyaudio
+
+
+def mic_in_spkr_out(effect_class, frequency, duration=10, **kwargs):
+    """
+    play the specified effect using microphone input and will output to speaker
+    """
+
+    # sound properties
+    BLOCKLEN = 1024  # Number of frames per block
+    WIDTH = 2  # Bytes per sample
+    CHANNELS = 1  # Number of channels
+    RATE = 8000  # Sampling rate in Hz (samples/second)
+
+    # implement effect
+    effect = effect_class(frequency=frequency / RATE, **kwargs)
+
+    # Open the audio output stream
+    p = pyaudio.PyAudio()
+    PA_FORMAT = p.get_format_from_width(WIDTH)
+    stream = p.open(format=PA_FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=256)
+
+    print('start playing for %f seconds ...' % duration)
+
+    # Loop through blocks
+    for i in range(int(duration * RATE / BLOCKLEN)):
+        input_bytes = stream.read(BLOCKLEN, exception_on_overflow=False)
+        x = struct.unpack('h' * BLOCKLEN, input_bytes)
+        y = effect.cal_output(x)
+
+        y = np.clip(y.astype(np.int), -32768, 32767)
+
+        # Convert numeric list to binary data
+        output_bytes = struct.pack('h' * BLOCKLEN, *y)
+
+        # Write binary data to audio output stream
+        stream.write(output_bytes, BLOCKLEN)
+
+    print('* Finished')
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
